2debc1b06fca68f63c7f2072724ceabf5499cc73
Dennis Muturi <denismutur@gmail.com>
2023-05-20 19:14:24 +0300

    Edit my js to add a toggle button

--- etchasketch.js
+++ etchasketch.js
@@ -1,6 +1,11 @@
 const container=document.querySelector('.container');
-let squarecount=160;
+let squarecount=40;
 let myBoolean=false;
+let myBoolean2=false;
+
+
+
+
 
 
     
@@ -43,12 +48,13 @@ function handleInputChange() {
 
 // Add an event listener to handle the input change
 myinput.addEventListener('input',function(){
-    noOfSquares.textContent=this.value;
+    noOfSquares.textContent=this.value+'X'+this.value;
     container.innerHTML='';
 
     handleInputChange();
+    if(myBoolean){
     toggleButton.checked = !toggleButton.checked;
-    myBoolean=!myBoolean;
+    myBoolean=!myBoolean;}
     
     
    
@@ -64,8 +70,10 @@ colorPicker.addEventListener('input', function() {
   square1.forEach(square2 => square2.addEventListener('mouseover',function(){
       square2.style.backgroundColor =color;
   }))
+  if(myBoolean){
   toggleButton.checked = !toggleButton.checked;
-  myBoolean=!myBoolean;
+  myBoolean=!myBoolean;}
+
   
 
 
@@ -79,7 +87,7 @@ function generateRandomColor(){
   let randColor = randomNumber.padStart(6, 0);   
   return `#${randColor.toUpperCase()}`
 }
-console.log(generateRandomColor()); 
+
 const toggleButton = document.querySelector('.toggle-button input[type="checkbox"]');
 toggleButton.addEventListener('change', function() {
   myBoolean=!myBoolean;
@@ -92,6 +100,61 @@ toggleButton.addEventListener('change', function() {
 
   
 });
+const shaderButton = document.querySelector('.shader-button input[type="checkbox"]');
+shaderButton.addEventListener('change', function() {
+
+myBoolean2=!myBoolean2;
+//alert('wait a few seconds for shader to change');
+
+
+   if(myBoolean2){
+    const square2=document.querySelectorAll('.square');
+    square2.forEach(item =>{
+      item.style.opacity='0.1';
+    });
+    
+  square2.forEach(square2 => square2.addEventListener('mouseover',function(){
+    
+    let myvar1=getCurrentOpacity(this);
+    
+      
+    
+    
+  
+
+   {
+      
+      changeStyle(this,myvar1);
+    
+
+      
+    }
+
+  }))
+ 
+
+  
+}
+if(myBoolean2===false){
+  const square2=document.querySelectorAll('.square');
+    square2.forEach(item =>{
+      item.style.opacity='1';
+    });
+
+}
+
+
+});
+function changeStyle(element,myvar){
+  myvar=myvar+0.1;
+  element.style.opacity =myvar;
+}
+function getCurrentOpacity(element) {
+  const computedStyle = window.getComputedStyle(element);
+  const opacity = computedStyle.getPropertyValue('opacity');
+  const number = parseFloat(opacity);
+  return number;
+}
 
 
 
